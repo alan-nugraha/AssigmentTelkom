f78eb2d2d3a97a7275486badd137816d9169cd47
Alan Nugraha <yaelahlan@gmail.com>
2021-06-26 00:19:40 +0700

    update

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {Home} from './src';
+import React from 'react';
+import Router from './src/routes';
 
-const Stack = createStackNavigator();
-
-const routeStack = () => {
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+      <Router />
     </NavigationContainer>
   );
 };
 
-export default routeStack;
+export default App;
