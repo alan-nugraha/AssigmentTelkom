32595a262a374b535cd36965322c9fd1db5847d7
Alan Nugraha <yaelahlan@gmail.com>
2021-06-28 17:54:10 +0700

    add color

--- src/styles/Color.js
+++ src/styles/Color.js
@@ -5,8 +5,11 @@ export const Color = {
   BLUE_DARK: '#0081a0',
   GREEN: '#E3FFE8',
   GREY: '#B8B8B8',
+  GREY2: '#636365',
   ORANGE: '#FF5200',
+  ORANGE2: '#f7ad3f',
   RED: '#e52a39',
   WHITE: '#FFFFFF',
   WHITE2: '#FAFAFA',
+  WHITE3: '#fafbfd',
 };
